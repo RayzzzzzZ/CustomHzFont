bc76700a714946c5eafe7e4da2e24fcb9bd3dff2
Ray______ <393723821@qq.com>
2019-09-30 10:20:40 +0800

    upload files

---
+++ CustomFontTool/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CustomFontTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.createLibBtn = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(22, 30);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(533, 21);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // createLibBtn
+            // 
+            this.createLibBtn.Location = new System.Drawing.Point(583, 30);
+            this.createLibBtn.Name = "createLibBtn";
+            this.createLibBtn.Size = new System.Drawing.Size(105, 33);
+            this.createLibBtn.TabIndex = 2;
+            this.createLibBtn.Text = "生成";
+            this.createLibBtn.UseVisualStyleBackColor = true;
+            this.createLibBtn.Click += new System.EventHandler(this.createLibBtn_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(22, 118);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(533, 252);
+            this.outputTextBox.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(583, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 182);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "数据";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 393);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.createLibBtn);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "CustomFont";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button createLibBtn;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CustomFontTool/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomFontTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        //字符串转bitmap
+        private Bitmap StringToBitmap(String str)
+        {
+            StringFormat sf = new StringFormat();
+
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Near;
+
+            Bitmap bmp = new Bitmap(16, 16);
+            Graphics g = Graphics.FromImage(bmp);
+            Font f = new Font("宋体", 16 * 3 / 4);
+            g.DrawString(str, f, Brushes.Black, new Rectangle(0, 0, 16, 16), sf);
+
+            return bmp;
+        }
+
+        //bitmap转16进制点阵数
+        //暂用列行式
+        private byte[,] BitmapToCharArray(Bitmap bmp)
+        {
+            int row;
+            int y = bmp.Height;
+
+            if (y % 8 > 0)
+                row = y / 8 + 1;
+            else
+                row = y / 8;
+
+            byte[,] dat = new byte[row, bmp.Width];//创建数组
+
+            for( int i = 0; i < row; i ++)//遍历每一行的“列行式”数据
+            {
+                //遍历每一列8像素点
+                for( int x = 0; x < bmp.Width; x ++)
+                {
+                    for( int line = 0; line < 8; line ++)
+                    {
+                        if (bmp.GetPixel(x, i * 8 + line) == Color.FromArgb(0, 0, 0))
+                            dat[i, x] |= (byte)(1<<(line));
+                    }
+                }
+            }
+
+            return dat;
+        }
+
+        //Byte二维数组转成单汉字的字符串
+        private string ByteDArrayToString(byte [,]dat)
+        {
+            StringBuilder str = new StringBuilder();
+            int num = 0;
+
+            for (int i = 0; i < dat.GetLength(0); i++)
+            {
+                for (int j = 0; j < dat.GetLength(1); j++)
+                {                    
+                    str.Append("0x" + dat[i, j].ToString("x2"));
+                    num++;
+                    str.Append(",");
+                    if (num % 16 == 0)//每16个换下行
+                        str.Append("\r\n");                   
+                }
+            }
+
+            return str.ToString();
+        }
+
+        private void createLibBtn_Click(object sender, EventArgs e)
+        {
+            string str;
+
+            if (inputTextBox.Text != "")
+            {
+                str = inputTextBox.Text;
+                char[] cr = str.ToCharArray();
+
+
+                outputTextBox.Text = "";//Clear
+                                        //遍历
+                for (int i = 0; i < cr.Length; i++)
+                {
+                    Bitmap bmp = StringToBitmap(cr[i].ToString());
+                    byte[,] dat = BitmapToCharArray(bmp);
+                    string hex_data = ByteDArrayToString(dat);
+
+                    outputTextBox.Text += "\"" + cr[i].ToString() + "\",\r\n" + hex_data;
+                }
+            }
+            
+
+            
+
+            //pictureBox1.Image = Image.FromHbitmap(bmp.GetHbitmap());
+            //pictureBox1.Show();
+        }
+    }
+}
